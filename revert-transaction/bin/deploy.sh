8fbef4d46093768767f5333857c15eab9e3faf9e
adachi_440 <tomoki1999314@gmail.com>
2025-01-26 20:02:07 +0400

    add revert transaction

---
+++ revert-transaction/bin/deploy.sh
@@ -0,0 +1,1 @@
+forge script script/RevertTransaction.s.sol --rpc-url http://localhost:8547 --broadcast --legacy -vvvv --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
